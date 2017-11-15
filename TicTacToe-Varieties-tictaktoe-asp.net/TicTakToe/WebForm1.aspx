22b03e576acebaef1dd00c5c8ac0031980b13641
Deependra Tripathi <dtripathi@tavisca.com>
2017-11-15 17:43:19 +0530

    added complete tictactoe game

---
+++ TicTacToe-Varieties-tictaktoe-asp.net/TicTakToe/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TicTakToe.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+        <asp:Button ID="A1" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text=" " Font-Size="Medium" />
+        <asp:Button ID="A2" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text=" " Font-Bold="True" Font-Size="Medium" />
+        <asp:Button ID="A3" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text=" " Font-Bold="True" Font-Size="Medium" />
+       </p>
+            <p>
+            <asp:Button ID="B1" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text=" " Font-Bold="True" Font-Size="Medium" />
+            <asp:Button ID="B2" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text=" " Font-Bold="True" Font-Size="Medium" />
+            <asp:Button ID="B3" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text=" " Font-Bold="True" Font-Size="Medium" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;<asp:Label ID="Message" runat="server" Height="32px" SkinID="50" Width="100px" BorderWidth="5px" Font-Bold="True">Message</asp:Label>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </p>
+        <p>
+            <asp:Button ID="C1" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text=" " Font-Bold="True" Font-Size="Medium" />
+            <asp:Button ID="C2" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text=" " Font-Bold="True" Font-Size="Medium" />
+            <asp:Button ID="C3" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text=" " Font-Bold="True" Font-Size="Medium" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+        <p>
+            &nbsp;</p>
+        <div style="margin-left: 80px">
+&nbsp;&nbsp;
+            <asp:Button ID="Restart" runat="server" BorderWidth="5px" OnClick="Restart_Click" Text="Rematch" Width="148px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ TicTacToe-Varieties-tictaktoe-asp.net/TicTakToe/WebForm1.aspx.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TicTakToe
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public static int player = 2;
+        public int s1=0;
+        public int s2 = 0;
+        public int sd = 0;
+
+        protected void Page_Load(object OBJ, EventArgs e)
+        {
+            Message.Text = "Player X Turn";
+        }
+
+        private void disableButtons()
+        {
+            A1.Enabled = A2.Enabled = A3.Enabled = B1.Enabled = B2.Enabled = B3.Enabled = C1.Enabled = C2.Enabled = C3.Enabled = false;
+        }
+        protected void ButtonClick(object sender, EventArgs e)
+        {
+            Button button =(Button) sender;
+
+            if (button.Text == " ")
+            {
+                if (player % 2 == 0)
+                {
+                    Message.Text = "Player O Turn !";
+                    button.Text = "X";
+                    if(Winner()== true)
+                    {
+                        Message.Text = "Player O Is The Winner !";
+                        disableButtons();
+                    }
+                    player++;
+                }
+                else
+                {
+                    Message.Text = "Player X Turn !";
+                    button.Text = "O";
+                    if (Winner() == true)
+                    {
+                        Message.Text = "Player X Is The Winner !";
+                        disableButtons();
+                    }
+                    player++;
+                }
+                if(Draw()==true)
+                {
+                    Message.Text = "Match is Draw.!!!!";
+                }
+            }
+        }
+        protected bool Winner()
+        {
+            if(A1.Text== A2.Text && A2.Text==A3.Text && A1.Text!=" ")
+            {
+                return true;
+            }
+            if (B1.Text == B2.Text && B2.Text == B3.Text && B1.Text != " ")
+            {
+                return true;
+            }
+            if (C1.Text == C2.Text && C2.Text == C3.Text && C1.Text != " ")
+            {
+                return true;
+            }
+            if (A1.Text == B1.Text && B1.Text == C1.Text && A1.Text != " ")
+            {
+                return true;
+            }
+            if (A2.Text == B2.Text && B2.Text == C2.Text && A2.Text != " ")
+            {
+                return true;
+            }
+            if (A3.Text == B3.Text && B3.Text == C3.Text && A3.Text != " ")
+            {
+                return true;
+            }
+            if (A1.Text == B2.Text && B2.Text == C3.Text && A1.Text != " ")
+            {
+                return true;
+            }
+            if (A3.Text == B2.Text && B2.Text == C1.Text && A3.Text != " ")
+            {
+                return true;
+            }
+          return false;
+        }
+        public bool Draw()
+        {
+            if(A1.Text!=" " && A2.Text != " " && A3.Text != " " && B1.Text != " " && B2.Text != " " && B3.Text != " " && C1.Text != " " && C2.Text != " " && C3.Text != " " )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        protected void Restart_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebForm1.aspx");
+        }
+    }
+    }
